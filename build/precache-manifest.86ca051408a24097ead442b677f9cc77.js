2c747f848b99ebfb2378e9c987c4aa2ee759aae1
rushikesh <rushi3691@outlook.com>
2022-07-21 21:17:46 +0530

    v1.0.1

---
+++ build/precache-manifest.86ca051408a24097ead442b677f9cc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670175517463b4841720fd9dea2f8b9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814186e4a286bda25556",
+    "url": "/static/css/main.19195279.chunk.css"
+  },
+  {
+    "revision": "2c847c28d51a4037bf3f",
+    "url": "/static/js/2.ca6c4a95.chunk.js"
+  },
+  {
+    "revision": "8b78294f34894140d628da3466e7f53b",
+    "url": "/static/js/2.ca6c4a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814186e4a286bda25556",
+    "url": "/static/js/main.5f41115b.chunk.js"
+  },
+  {
+    "revision": "d543f5d3dc78e0ef6489",
+    "url": "/static/js/runtime-main.79d754ed.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,19 +7,12 @@ import {v4 as uuid} from "uuid";
 const LOCAL_STORAGE_KEY = 'todo_key';
 function App() {
   const [text, setText] = useState('');
+  const [group, setGroup] = useState('');
   const [state, setState] = useState({
     "todo": {
       title: "Todo",
       items: []
     },
-    "in-progress": {
-      title: "In Progress",
-      items: []
-    },
-    "done": {
-      title: "Completed",
-      items: []
-    }
   })
 
   useEffect(()=>{
@@ -50,23 +43,59 @@ function App() {
 
   }
   const addItem = () => {
-    setState(prev => {
-      return {
-        ...prev,
-        todo: {
-          title: "Todo",
-          items: [
+    if(!state){
+      const newState = {
+        "todo":{
+          title:"Todo",
+          items:[
             {
               id: uuid(),
               name: text
-            },
-            ...prev.todo.items
+            }
           ]
         }
       }
-    })
+      setState(newState);
+    }else{
+      setState(prev => {
+        return {
+          ...prev,
+          todo: {
+            title: "Todo",
+            items: [
+              {
+                id: uuid(),
+                name: text
+              },
+              ...prev.todo.items
+            ]
+          }
+        }
+      })
+    }
 
     setText("")
+  }
+
+  const addGroup = () =>{
+    if (group in state){
+      return;
+    }
+    setState(prev => {
+      return {
+        ...prev,
+         [group]: {
+          title: group,
+          items: []
+        }
+      }
+    })
+  }
+
+  const deleteGroup = (userkey) =>{
+    let newState = Object.assign({}, state);
+    delete newState[userkey]
+    setState(newState)
   }
   const deleteItem = ({key, index}) =>{
     let newState = Object.assign({}, state);
@@ -77,13 +106,28 @@ function App() {
   return (
     <div className="App">
       <div>
+        <label>Add Note</label>
         <input type="text" value={text} onChange={(e)=>setText(e.target.value) }/>
         <button onClick={addItem}>Add</button>
+      </div>
+      <div>
+        <label>Add Group</label>
+        <input type="text" value={group} onChange={(e)=>setGroup(e.target.value) }/>
+        <button onClick={addGroup}>Add</button>
       </div>
       <DragDropContext onDragEnd={handleDragEng}>
         {_.map(state, (data, key) => {
           return(
             <div key={key} className={"column"}>
+              {key!=="todo" ?
+              (<div onClick={()=>deleteGroup(key)}
+                  className="close-group">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                  </svg>
+              </div>):
+              <></>
+              }
               <h3>{data.title}</h3>
               <Droppable droppableId={key}>
                 {(provided) => {
